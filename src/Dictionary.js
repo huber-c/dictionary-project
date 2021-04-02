a9f224572896a3055e955d6829b2e170c65b3e9e
Carina Huber <huber.carina2806@gmail.com>
2021-04-02 14:32:23 +0100

    Add Results

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [entry, setEntry] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -27,6 +31,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={updateEntryChange} autoFocus={true} />
       </form>
+      <Results />
     </div>
   );
 }
