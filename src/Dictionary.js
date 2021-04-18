14d9b7cc210d450c48395b129bd0fb8c9b40baf8
Carina Huber <huber.carina2806@gmail.com>
2021-04-18 20:57:31 +0100

    Configure image examples

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,13 +2,19 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [entry, setEntry] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePixabayResponse(response) {
+    setPhotos(response.data.hits);
   }
 
   function search(event) {
@@ -16,8 +22,10 @@ export default function Dictionary() {
 
     //https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${entry}`;
-    console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
+    let pixabayApiKey = "21219677-e437494d5d8ae28cf2eb1cfab";
+    let pixabayApiUrl = `https://pixabay.com/api/?key=21219677-e437494d5d8ae28cf2eb1cfab&q=${entry}&image_type=photo&api_key=${pixabayApiKey}&per_page=9`;
+    axios.get(pixabayApiUrl).then(handlePixabayResponse);
   }
 
   function updateEntryChange(event) {
@@ -41,6 +49,7 @@ export default function Dictionary() {
         </div>
       </div>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
